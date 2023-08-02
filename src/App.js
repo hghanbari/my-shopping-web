d446189a8fead0b7cb3f0bca6146ee510b5536fd
hghanbari <gh.hamid89@gmail.com>
2023-08-02 17:44:39 +0200

    Create Filter Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import data from "./data.json";
 import Products from "./components/Products";
+import Filter from "./components/Filter";
 export default class App extends Component {
   constructor() {
     super();
@@ -10,6 +11,45 @@ export default class App extends Component {
       sort: "",
     };
   }
+
+  filterProducts = (event) => {
+    if (event.target.value === "") {
+      this.setState({
+        size: event.target.value,
+        products: data.products,
+      });
+    } else {
+      this.setState({
+        size: event.target.value,
+        products: data.products.filter(
+          (product) => product.availableSizes.indexOf(event.target.value) >= 0
+        ),
+      });
+    }
+  };
+
+  sortProducts = (event) => {
+    const sort = event.target.value;
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+          sort === "lowest"
+            ? a.price > b.price
+              ? 1
+              : -1
+            : sort === "highest"
+            ? a.price < b.price
+              ? 1
+              : -1
+            : a._id < b.price
+            ? 1
+            : -1
+        ),
+    }));
+  };
+
   render() {
     return (
       <div className="grid-container">
@@ -19,6 +59,13 @@ export default class App extends Component {
         <main>
           <div className="content">
             <div className="main">
+              <Filter
+                count={this.state.products.length}
+                size={this.state.size}
+                sort={this.state.sort}
+                filterProducts={this.filterProducts}
+                sortProducts={this.sortProducts}
+              />
               <Products products={this.state.products} />
             </div>
             <div className="sidebar">Cart Item</div>
